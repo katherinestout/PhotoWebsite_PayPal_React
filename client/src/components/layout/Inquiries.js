d8314ff273f1076c15b76095f672888c3077027f
katherinestout <kjs14@hood.edu>
2019-02-27 13:00:40 -0500

    creating form

--- client/src/components/layout/Inquiries.js
+++ client/src/components/layout/Inquiries.js
@@ -1,10 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Inquiries extends Component {
+  constructor(){
+    super();
+    this.state ={
+      name: 'Your Name',
+      email: 'Email@email.com',
+      message: 'Your Message'
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(this.state.value);
+  }
+
+
   render() {
     return (
-      <div>
+      <div className="inquiries">
           <h1>Inquiries</h1>
+          <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" 
+          onChange={this.handleChange}
+          name="name" />
+        </label>
+
+        <label>
+          Email:
+          <input type="email"
+          onChange={this.handleChange}
+          name="email"/>
+        </label>
+
+        <label>
+          Message:
+          <textarea onChange={this.handleChange} 
+          type="textarea"
+          name="message"/>
+        </label>
+
+
+
+
+        <input 
+        type="submit" 
+        value="Submit" />
+      </form>
         
       </div>
     )
