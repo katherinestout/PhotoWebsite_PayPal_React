ee28e962aa71e8f97554a2d7268fcea923be9562
katherinestout <kjs14@hood.edu>
2019-03-13 11:55:27 -0400

    can render links diff way other than turnery

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -16,18 +16,25 @@ class NavBar extends Component {
     this.props.clearCurrentProfile();
   }
 
+  //{isAthenticated && isAdmin ? adminLinks : guestlinks}
+/* if (isAuthenticated && isAdmin){
+      adminLinks
+} else if (isAuthenticated){
+  authLinks
+} else {
+  guestLinks
+}
+*/
+// if (user.email === admin@gmail.com){adminLinks} else {authLinks}
+
   render() {
 
     const {isAuthenticated, user} = this.props.auth;
-    //const {profile} = this.props.profile;
-    //const {email} = this.props.profile;
-   //const{profile} = this.props.profile;
+  
    console.log(this.props.auth.user.email);
- 
-  
-   
-  
 
+  
+//guest links
     const guestLinks = (
       
       <nav>
@@ -59,7 +66,11 @@ class NavBar extends Component {
       </ul>
       </nav>
     );
+
+    //auth links
     const authLinks = (
+     
+
       <nav>
       <ul className="navbar-nav mr-auto">
       <li className="nav-item">
@@ -70,10 +81,6 @@ class NavBar extends Component {
          <b>{user.name}</b> 
           </Link>
       
-        <Link className="button" to="/feed"
-              style={{ textDecoration: 'none', color: 'whitesmoke'}}>
-          <b> Quotes Feed</b>
-          </Link>
 
           <Link className="button" to="/request"
               style={{ textDecoration: 'none', color: 'whitesmoke'}}>
@@ -99,16 +106,61 @@ class NavBar extends Component {
       </ul>
       </nav>
     );
+//admin links
+    const adminLinks = (
+      <nav>
+      <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
 
+
+      <Link className="button" to={`/profile/${this.props.auth.user.email}`}  
+              style={{ textDecoration: 'none', color: 'whitesmoke'}}>
+         <b>{user.name}</b> 
+          </Link>
+      
+        <Link className="button" to="/feed"
+              style={{ textDecoration: 'none', color: 'whitesmoke'}}>
+          <b> Quotes Feed</b>
+          </Link>
+
+          <Link className="button" to="/calendar" 
+              style={{ textDecoration: 'none', color: 'whitesmoke'}}>
+         <b> Calendar</b> 
+          </Link>
+
+          <a href="/home" onClick={this.onLogoutClick.bind(this)} className="button"
+          style={{ textDecoration: 'none', color: 'whitesmoke'}}>
+          <b>logout</b>
+          </a>
+      </li>
+      </ul>
+      </nav>
+    );
+
+    const renderLinks = () => {
+      if (isAuthenticated){
+       return authLinks
+
+      } else {
+       return guestLinks;
+      }
+   
+    } 
+
+//   {isAuthenticated ? authLinks : guestLinks}
     return (
       <div>
         
-       {isAuthenticated ? authLinks : guestLinks}
+     
+       {renderLinks()}
         
       </div>
     )
   }
 }
+
+
+
 NavBar.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
---
+++ client/src/components/layout/Yep.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Yep = () => {
+  return (
+    <div>
+      <Link to ="/editprofile" className="btn btn-dark">Edit Profile</Link>
+    
+    </div>
+  );
+};
+
+export default Yep;
