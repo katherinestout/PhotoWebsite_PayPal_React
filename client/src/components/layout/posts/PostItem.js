46293fdf333534bd4155c7250c0b106c8a24d35e
katherinestout <kjs14@hood.edu>
2019-03-26 15:32:57 -0400

    cleaning up

--- client/src/components/layout/posts/PostItem.js
+++ client/src/components/layout/posts/PostItem.js
@@ -8,88 +8,30 @@ import {deletePost,
 import './postitem.css';
 
  class PostItem extends Component {
-   //state = {
-     //clickedArray: []
-   //}
-     
+
      onDeleteClick(id){
          this.props.deletePost(id);
      }
 
-    
      onLikeClick(id){
       this.props.addLike(id);
-   
   }
-/*
-  handleClick(likes){
-    let clickedArray = this.state.clickedArray;
-    
-    const auth = this.props;
-//if the likes are greater than one, push to clicked array
-    if(likes.filter(like => like.user === auth.user.id).length > 0){
-      clickedArray.push(auth.user.id) && console.log(clickedArray);
-    } else {
-      return false;
-    }
-   
-  }
-
-*/
-/*
-handleClick(likes, post){
-  let clickedArray = this.state.clickedArray;
-  const auth = this.props;
-  //if post.likes is greater than 0 add to clicked array
-  if(post.likes > 0){
-    clickedArray.push(auth.user.id) && console.log('clicked arr' + clickedArray);
-
-  } else {
-    console.log('dunno');
-  }
-}
-*/
-  //function that triggers the confirmed and adds to clickedArray
-     clickWrapper = (id) =>{
-      this.onLikeClick(id);
-      //this.handleClick(id);
-     }
-
-     //find out if user liked already
 
      findUserLike(likes){
 
          const {auth} = this.props;
-         //let clickedArray = this.state.clickedArray;
 
          if(likes.filter(like => like.user === auth.user.id).length > 0){
             return true;
-            // && clickedArray.push(auth.user.id) && console.log(clickedArray);
          } else {
-          //clickedArray.push(auth.user.id) && console.log('clicked array' + clickedArray);
           return false;
          }
-  
      }
 
-     /*
-     confirmedUnconfirmed(likes){
-      let clickedArray = this.state.clickedArray;
-      //if a post has a like or more than one like, add it to confirmed, console log
-      //else, add it to unconfirmed
-      if(Posts.filter){
-        unconfirmedArray.push(auth.user.id) && console.log('clicked array' + unconfirmedArray);
-      } else {
-        clickedArray.push(auth.user.id) && console.log('clicked array' + clickedArray);
-      }
-       
-     }
-*/
- 
 
   render() {
-      const {post, auth} = this.props;
 
+      const {post} = this.props;
 
     return (
      
@@ -115,7 +57,7 @@ handleClick(likes, post){
 
             
                 <button
-                  onClick={this.clickWrapper.bind(this, post._id)} 
+                  onClick={this.onLikeClick.bind(this, post._id)} 
                   type="button"
                   className= {classnames("btn", {'btn-info': this.findUserLike(post.likes)})}
                 >
@@ -143,7 +85,7 @@ PostItem.propTypes = {
     deletePost: PropTypes.func.isRequired,
     addLike: PropTypes.func.isRequired,
     post: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
