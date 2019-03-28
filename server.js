6373de77b29808879d1db10afbc79588f1d768d3
katherinestout <kjs14@hood.edu>
2019-03-28 14:37:50 -0400

    prepared for deploy

--- server.js
+++ server.js
@@ -88,20 +88,19 @@ app.post('/api/form', (req, res) => {
     })
 
 });
-
-//server static assets if in production
+// Server static assets if in production
+//Check to see if on server, on heroku
 if(process.env.NODE_ENV === 'production'){
-    //set static folder
+    //set a static folder
     app.use(express.static('client/build'));
-
+    //route
+    //for any route that gets hit we're going to load the react index.html file
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-
 const port = process.env.PORT || 5000;
 
-//console.log(process.env.client_id);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
