e1c2bc7bdeae1e064d381a82b0de8bd7f782be7d
katherinestout <kjs14@hood.edu>
2019-03-28 14:12:58 -0400

    prepared for deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
 const passport = require('passport');
+const path = require('path');
 
 //routes
 const profile = require('./routes/api/profile');
@@ -87,6 +88,16 @@ app.post('/api/form', (req, res) => {
     })
 
 });
+
+//server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 const port = process.env.PORT || 5000;
