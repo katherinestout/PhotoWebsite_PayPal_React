6018b6c9f83450201a3243784482400e6b47bf2f
katherinestout <kjs14@hood.edu>
2019-04-20 17:27:18 -0400

    keys

--- config/keys.dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://katekate:katekate@pppcluster-shard-00-00-bkyo0.mongodb.net:27017,pppcluster-shard-00-01-bkyo0.mongodb.net:27017,pppcluster-shard-00-02-bkyo0.mongodb.net:27017/test?ssl=true&replicaSet=PPPcluster-shard-0&authSource=admin&retryWrites=true',
-    secretOrKey: 'secret'
-};
